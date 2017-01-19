c5aa8a6eb08614dd2acda07d7625790cf7f62909
YTYanK <ringht@vip.qq.com>
2017-01-19 16:44:16 +0800

    － 修改

---
+++ FastContact/Classes/Views/Personal/ModifyPassword/ModifyPasswordVC.m
@@ -0,0 +1,37 @@
+//
+//  ModifyPasswordVC.m
+//  FastContact
+//
+//  Created by YTYanK on 17/1/19.
+//  Copyright © 2017年 YTYanK. All rights reserved.
+//
+
+#import "ModifyPasswordVC.h"
+
+@interface ModifyPasswordVC ()
+
+@end
+
+@implementation ModifyPasswordVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
